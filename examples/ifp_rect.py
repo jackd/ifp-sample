d506204da70f2b33c4301b2378b0d623c2a4802a
Dominic Jack <thedomjack@gmail.com>
2020-03-10 15:55:12 +1000

    tweaks

--- examples/ifp_rect.py
+++ examples/ifp_rect.py
@@ -3,8 +3,9 @@ from __future__ import division
 from __future__ import print_function
 
 import numpy as np
-from scipy.spatial import cKDTree as KDTree  # pylint: disable=no-name-in-module
+import numba
 import heapq
+from scipy.spatial import cKDTree as KDTree  # pylint: disable=no-name-in-module
 from timeit import timeit
 import functools
 
@@ -43,7 +44,7 @@ def ifp_sample_base(indices, dists, out_size):
     if out_size > in_size:
         raise ValueError('Requires out_size <= in_size, but {} > {}'.format(
             out_size, in_size))
-    i = np.arange(in_size, dtype=np.int64)
+    i = np.arange(in_size, dtype=np.uint32)
     heap_dists = np.empty((in_size,), dtype=np.float32)
     heap_dists[:] = -np.inf
     heap = list(zip(heap_dists, i))
@@ -73,6 +74,61 @@ def ifp_sample_base(indices, dists, out_size):
     return out
 
 
+@numba.njit()
+def _ifp_sample_numba(indices, dists, heap_dists, i, out):
+    """
+    Args:
+        indices: [in_size, K] ints in [0, in_size)
+        dists: [in_size, K] floats
+        out_size: int, (maximum) number of outputs
+
+    Returns:
+        indices: [out_size] int indices of sampled points.
+    """
+    heap = list(zip(heap_dists, i))
+    count = 0
+    out_size = out.shape[0]
+
+    while heap:
+        dist, index = heapq.heappop(heap)
+        if dist != heap_dists[index]:
+            continue
+        out[count] = index
+        count += 1
+        if count >= out_size:
+            break
+        di = dists[index]
+        ii = indices[index]
+        new_heap_dists = heap_dists[ii]
+        needs_updating = di > new_heap_dists
+        ii = ii[needs_updating]
+        di = di[needs_updating]
+        heap_dists[ii] = di
+        heap_dists[index] = 0
+        for item in zip(di, ii):
+            heapq.heappush(heap, item)
+    else:
+        raise RuntimeError('Should have broken...')
+    return out
+
+
+def ifp_sample_numba(indices, dists, out_size):
+    indices = indices.astype(np.uint32)
+    dists = dists.astype(np.float32)
+    # remove self-dists / make dists negative
+    dists = -dists[:, 1:]
+    indices = indices[:, 1:]
+    out = -np.ones((out_size,), dtype=np.int64)
+    in_size, _ = indices.shape
+    if out_size > in_size:
+        raise ValueError('Requires out_size <= in_size, but {} > {}'.format(
+            out_size, in_size))
+    i = np.arange(in_size, dtype=np.uint32)
+    heap_dists = np.full((in_size,), -np.inf, dtype=np.float32)
+    # heap = list(zip(heap_dists, i))
+    return _ifp_sample_numba(indices, dists, heap_dists, i, out)
+
+
 np.random.seed(123)
 x = np.random.uniform(size=(in_size, 2)).astype(dtype=np.float32)
 tree = KDTree(x)
@@ -83,13 +139,15 @@ kwargs = dict(indices=indices, dists=dists, out_size=out_size)
 fn_pq = functools.partial(ifp_sample_pq, **kwargs)
 fn_hybrid = functools.partial(ifp_sample_hybrid, **kwargs)
 fn_base = functools.partial(ifp_sample_base, **kwargs)
+fn_numba = functools.partial(ifp_sample_numba, **kwargs)
 
 run_benchmarks(burn_iters, num_iters, ('pq', fn_pq), ('hybrid', fn_hybrid),
-               ('base', fn_base))
+               ('base', fn_base), ('numba', fn_numba))
 
 pq_indices = fn_pq()
 hybrid_indices = fn_hybrid()
 base_indices = fn_base()
+numba_indices = fn_numba()
 # print(np.stack((base_indices, pq_indices, hybrid_indices), axis=1))
 # print(['base', 'pq', 'hybrid'])
 print('All same (pq vs base)    : {}'.format(
@@ -98,6 +156,8 @@ print('All same (hybrid vs base): {}'.format(
     np.all(hybrid_indices == base_indices)))
 print('All same (pq vs hybrid)  : {}'.format(
     np.all(pq_indices == hybrid_indices)))
+print('All same (numba vs base)  : {}'.format(
+    np.all(numba_indices == base_indices)))
 
 import matplotlib.pyplot as plt
 x_out = x[pq_indices]
--- examples/rej_rect.py
+++ examples/rej_rect.py
@@ -6,15 +6,40 @@ import numpy as np
 from scipy.spatial import cKDTree as KDTree  # pylint: disable=no-name-in-module
 from timeit import timeit
 import functools
+# import numba
 
 from ifp import rejection_sample
 from ifp.rej import rejection_sample_dists
 from ifp.rej import rejection_sample_dists_base
 from ifp.rej import rejection_sample_base
+from numba import jit
 
 from benchmark_util import run_benchmarks
 
-k = 8
+
+@jit(nopython=True)
+def _rejection_sample_numba(indices, consumed, out):
+    in_size = indices.shape[0]
+    max_size = out.shape[0]
+    count = 0
+    for i in range(in_size):
+        if count >= max_size:
+            break
+        if not consumed[i]:
+            consumed[indices[i]] = True
+            out[count] = i
+            count += 1
+    return out[:count]
+
+
+def rejection_sample_numba(indices, max_size):
+    in_size = indices.shape[0]
+    consumed = np.zeros((in_size,), dtype=np.bool)
+    out = np.empty((max_size,), dtype=np.uint32)
+    return _rejection_sample_numba(indices, consumed, out)
+
+
+k = 16
 in_size = 1024
 max_size = 512
 num_iters = 200
@@ -35,6 +60,7 @@ rej_base = functools.partial(rejection_sample_base, **kwargs)
 rej_dist_base = functools.partial(rejection_sample_dists_base,
                                   dists=dists,
                                   **kwargs)
+rej_numba = functools.partial(rejection_sample_numba, **kwargs)
 run_benchmarks(
     burn_iters,
     num_iters,
@@ -42,13 +68,17 @@ run_benchmarks(
     ('rej_dist', rej_dist),
     ('rej_base', rej_base),
     ('rej_dist_base', rej_dist_base),
+    ('rej_numba', rej_numba),
 )
 
 rej_indices = rej()
 base_indices = rej_base()
+numba_indices = rej_numba()
 _, alt_indices = rej_dist()
 print('All same (rej vs rej base) : {}'.format(
     np.all(rej_indices == base_indices)))
+print('All same (rej vs rej numba) : {}'.format(
+    np.all(rej_indices == numba_indices)))
 print('All same (rej-dist vs base): {}'.format(
     np.any(alt_indices == base_indices)))
 print(alt_indices.shape[0])
